a6f4831e942af87f2fc8f73df7776bf95956a8dc
merci2 <karganja@yahoo.de>
2025-10-30 03:54:39 +0100

    create AppRouter.jsx

--- npm1/src/App.jsx
+++ npm1/src/App.jsx
@@ -1,11 +1,26 @@
-import "@esri/calcite-components/dist/components/calcite-button";
-
+import "@esri/calcite-components/dist/components/calcite-button"
 
 function App() {
 
+  //defaults: works also without writing them
   return (
-    <calcite-button>Button</calcite-button>
+    <calcite-button
+      appearance="solid"
+      kind="brand"
+      scale="m"
+      split-child="false"
+      type="button"
+      width="auto"
+      calcite-hydrated
+
+      icon-start="script"
+      href="https://www.n-tv.de/"
+      target="_blank"
+    >
+      Open News
+    </calcite-button>
   )
 }
 
 export default App
+
