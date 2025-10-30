26d9703e3ead1342b5de1d66bcaa9c95d9c5f15a
merci2 <karganja@yahoo.de>
2025-10-30 04:43:37 +0100

    Shell+ShellPanel+Panel

--- npm1/src/pages/Home.jsx
+++ npm1/src/pages/Home.jsx
@@ -1,25 +1,27 @@
+import "@esri/calcite-components/dist/components/calcite-shell"
+import "@esri/calcite-components/dist/components/calcite-shell-panel"
+import "@esri/calcite-components/dist/components/calcite-panel"
 import "@esri/calcite-components/dist/components/calcite-button"
 
 function Home() {
 
   //defaults: works also without writing them
   return (
+    <calcite-shell>
+        <calcite-shell-panel slot="panel-start" position="start" id="shell-panel-start">
+            <calcite-panel>
     <calcite-button
-      appearance="solid"
-      kind="brand"
-      scale="m"
-      split-child="false"
-      type="button"
-      width="auto"
-      calcite-hydrated
-
-      icon-start="Home"
+      icon-start="home"//case sensitive! lowercase!
       href="https://developers.arcgis.com/calcite-design-system/"
       target="_blank"
     >
       Calcite Design System
     </calcite-button>
+    </calcite-panel>
+    </calcite-shell-panel>
+    </calcite-shell>
   )
 }
 
 export default Home
+
--- npm1/src/routes/AppRouter.jsx
+++ npm1/src/routes/AppRouter.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home'
 
 function AppRouter() {
   return (
@@ -11,4 +11,7 @@ function AppRouter() {
   );
 }
 
-export default AppRouter;
+export default AppRouter
+
+
+
